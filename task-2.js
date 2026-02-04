71bc41bdb81a4c7827a956de731f5b2c93fce144
codewithalif <programmingwithalif@gmail.com>
2026-02-04 23:21:16 +0600

    task-2 are complete

---
+++ task-2.js
@@ -0,0 +1,12 @@
+var math = 75.25;
+var biology = 65;
+var chemistry = 80;
+var physics = 35.45;
+var bangla = 99.50;
+var totalSubject = 5;
+
+var total_mark = ( math + biology + chemistry + physics + bangla);
+
+var avarage_mark = ( total_mark / totalSubject);
+
+console.log(avarage_mark.toFixed(2));
