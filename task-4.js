2df40fb7ce907b7192adc231967027356131497c
codewithalif <programmingwithalif@gmail.com>
2026-02-04 23:33:38 +0600

    Task-4 some explain are fixed

--- task-4.js
+++ task-4.js
@@ -2,3 +2,11 @@ var a = isNaN('11');
 console.log(a);
 var a = isNaN(2-10);
 console.log(a);
+
+// 1* var a = isNaN('11');
+// '11' is a string but can be a number → false
+
+// 2* var a = isNaN(2-10);
+// 2-10 = -8, a number → false
+
+// Note: isNaN() is true only if value is not a number.
