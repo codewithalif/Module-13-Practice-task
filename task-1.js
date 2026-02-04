cfd65255cccfd3e57c13ec581cb752751087b9cf
codewithalif <programmingwithalif@gmail.com>
2026-02-04 23:02:37 +0600

    first java script task practice complete

---
+++ task-1.js
@@ -0,0 +1,8 @@
+var I_have_money = 500; // I have that money
+var orangePrice = 200; // orange price 
+var Appleprice = 150;  // apple price
+
+var totalCost = orangePrice + Appleprice; 
+var returnMoney = I_have_money - totalCost;
+
+console.log(returnMoney);
